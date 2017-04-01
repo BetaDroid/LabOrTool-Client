52bf3752399b020832b170bd98480955005db2ba
Daniel <danielzarioiu@gmail.com>
2017-04-01 11:37:11 +0200

    Implemented the logout component

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _LoginS: LoginService) { }
 
   ngOnInit() {
+    this._LoginS.make_logout();
   }
-
 }
