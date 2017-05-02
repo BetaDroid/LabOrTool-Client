e5c06a2d5bb17ad8fdaebe3cd4f164bf00854c29
Daniel <danielzarioiu@gmail.com>
2017-05-02 18:08:13 +0200

    Changed the connection address

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConnectionService {
-  public LabOrTool: string = "http://127.0.0.1:5000";
+  //public LabOrTool: string = "http://ec2-52-56-173-141.eu-west-2.compute.amazonaws.com:5000";
+  public LabOrTool: string = "http://127.0.0.1:8081/api/v1";
 }
